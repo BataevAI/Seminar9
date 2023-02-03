f7721f8cc00bb9ac00bede02cddc3b7fcb73743d
Andrey Bataev <bataevai@gmail.com>
2023-02-03 23:57:28 +0400

    Exc.66

--- Seminar9/Exc.66/Program.cs
+++ Seminar9/Exc.66/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumber(int M, int N) 
+{
+        
+        if (M == N) return N;
+
+        return  N + SumNumber(M, N - 1);            
+                           
+}
+ 
+Console.WriteLine("Введите число M, а мы выведем сумму всех натуральныx чисел в промежутке от M до N");
+
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Итоговая сумма: {SumNumber(M, N)}"); 
