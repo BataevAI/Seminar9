190619e33000db747fb1157198c650510ef33260
Andrey Bataev <bataevai@gmail.com>
2023-02-04 00:39:53 +0400

    Exc.68

--- Seminar9/Exc.68/Program.cs
+++ Seminar9/Exc.68/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+
+
+long Akkerman (long n, long m) 
+
+{
+    long akk = 0;
+if (n == 0) return m + 1; 
+else if (m == 0) return Akkerman(n - 1, 1);
+
+else 
+{  
+    akk = Akkerman(n - 1, Akkerman(n, m - 1));
+}
+
+
+return akk;
+}
+Console.WriteLine("Введите число N и M в качестве аргументов функции Аккерамна, а мы рискнем ее посчитать");
+
+Console.WriteLine("Введите число N:");
+long N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число M:");
+long M = Convert.ToInt32(Console.ReadLine());
+
+long akker = Akkerman(N, M);
+Console.WriteLine($"Функция Аккермана при n = {N}, m = {M} будет равна: {akker}");
